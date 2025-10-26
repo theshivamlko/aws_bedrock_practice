21a06bddcdf4756f772565272a8f0943144fd842
Shivam Srivastava <shivamsrivastava.9@gmail.com>
2025-10-26 12:40:57 +0530

    elearning

---
+++ e_learning/lambda.py
@@ -0,0 +1,33 @@
+import boto3
+import json
+
+client_knowledge = boto3.client('bedrock-agent-runtime')
+
+
+def lambda_handler(event, context):
+    user_prompt = event['prompt']
+    print(user_prompt)
+
+    request_body = {
+        'input': {
+            'text': user_prompt
+        },
+        'retrieveAndGenerateConfiguration': {
+            'type': 'KNOWLEDGE_BASE',
+            'knowledgeBaseConfiguration': {
+                'knowledgeBaseId': 'ON35CYA45B',
+                'modelArn': 'arn:aws:bedrock:us-east-1::foundation-model/amazon.nova-micro-v1:0',
+                'retrievalConfiguration': {
+                    'vectorSearchConfiguration': {
+                        'numberOfResults': 5  # Adjust as needed
+                    }
+                }
+            }
+        }
+    }
+    response = client_knowledge.retrieve_and_generate(**request_body)
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps(response['output']['text'])
+    }
